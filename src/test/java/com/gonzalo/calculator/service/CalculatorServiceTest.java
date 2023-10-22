fa807995890a4ce77176d109a2db2ed63de3b39a
gonzalonaranjo <gon.naranjo91@gmail.com>
2023-10-22 16:28:19 +0200

    Calculator: Added calculator service test

--- src/test/java/com/gonzalo/calculator/service/CalculatorServiceTest.java
+++ src/test/java/com/gonzalo/calculator/service/CalculatorServiceTest.java
@@ -1,14 +1,17 @@
 package com.gonzalo.calculator.service;
 
+import com.gonzalo.calculator.client.TracerClient;
 import com.gonzalo.calculator.model.OperationType;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
+import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.math.BigDecimal;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 
@@ -19,11 +22,13 @@ class CalculatorServiceTest {
     private CalculatorServiceImpl service;
 
     @InjectMocks
-    private Tracer tracer;
+    private TracerClient tracerClient;
 
     @Test
     void calculateAddOperation(){
         BigDecimal expectedResult = new BigDecimal("20");
+
+        doNothingWhenTracerIsCalled();
 
         BigDecimal result = service.calculate(OperationType.ADD, BigDecimal.TEN, BigDecimal.TEN);
 
@@ -34,14 +39,20 @@ class CalculatorServiceTest {
     void calculateSubtractOperation() {
         BigDecimal five = new BigDecimal("5");
 
+        doNothingWhenTracerIsCalled();
+
         BigDecimal result = service.calculate(OperationType.SUBTRACT, BigDecimal.TEN, five);
 
         assertAndVerifyResult(five, result);
     }
 
+    private void doNothingWhenTracerIsCalled() {
+        Mockito.doNothing().when(tracerClient).trace(any());
+    }
+
     private void assertAndVerifyResult(BigDecimal expectedResult, BigDecimal result) {
         assertThat(result)
                 .isEqualTo(expectedResult);
-        verify(tracer, times(1)).trace(result);
+        verify(tracerClient, times(1)).trace(result);
     }
 }
