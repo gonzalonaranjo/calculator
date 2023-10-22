9737382dc3196b348410187297fa83955c9f472a
gonzalonaranjo <gon.naranjo91@gmail.com>
2023-10-22 13:52:10 +0200

    Calculator: Initial commit

---
+++ src/test/java/com/gonzalo/calculator/CalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gonzalo.calculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalculatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
